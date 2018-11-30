e02a05c035552e25037f96c735a43b50ba2cea23
gaosiqiang <gaosiqiang@zhongwenweilai.com>
2018-11-30 18:41:41 +0800

    fist commit

---
+++ app/controller/CommonController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sqtech
+ * Date: 2018/11/22
+ * Time: 6:08 PM
+ */
+
+namespace app\controller;
+
+use vendor\base\BaseController;
+
+class CommonController extends BaseController
+{
+
+}
